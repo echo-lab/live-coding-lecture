d275e504eb05b4e5c0ff7f6c36ff54e96477d8b6
Danny Manesh <danielmanesh@vt.edu>
2024-08-08 21:13:07 -0400

    7 second time limit for running code

--- src/client/code-runner.js
+++ src/client/code-runner.js
@@ -1,13 +1,20 @@
 // import { loadPyodide } from "pyodide";
 import { makeID } from "./utils";
-
+const MAX_RUNTIME = /*seconds=*/ 7 * 1000;
 
 export class PythonCodeRunner {
   constructor() {
     this.pyodide = null;
-    this.worker = new Worker(new URL("./pyodide-webworker.js", import.meta.url));
     this.callbacks = {};
+    this.restartWebWorker();
+  }
 
+  restartWebWorker() {
+    console.log("Restarting web worker");
+    this.worker?.terminate();
+    this.worker = new Worker(
+      new URL("./pyodide-webworker.js", import.meta.url)
+    );
     this.worker.onmessage = (event) => {
       const { id, ...data } = event.data;
       const onSuccess = this.callbacks[id];
@@ -18,6 +25,15 @@ export class PythonCodeRunner {
 
   asyncRun(code) {
     const id = makeID();
+
+    setTimeout(() => {
+      if (!this.callbacks[id]) return;
+      // We timed out if we got here.
+      this.callbacks[id]({ timedOut: true, error: "[CANCELLED DUE TO TIMEOUT]" });
+      delete this.callbacks[id];
+      this.restartWebWorker();
+    }, MAX_RUNTIME);
+
     return new Promise((onSuccess) => {
       this.callbacks[id] = onSuccess;
       this.worker.postMessage({
